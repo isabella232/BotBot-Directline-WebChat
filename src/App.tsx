c55afd0025db16d5ee9fa7ce4ac4d0b76cdd9f63
Thang Kieu <kqthang1505@gmail.com>
2018-02-07 16:17:00 +0700

    Add login form

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Chat, ChatProps } from './Chat';
 import * as konsole from './Konsole';
+import axios from 'axios';
+
+import Login from './Login';
 
 export type AppProps = ChatProps;
 const PRODUCTION_SHORT_URL = 'gicpublicsite.azurewebsites.net';
-const SECRET = {
-    STAGING: 'hWhA3IfZkGA.cwA.058.GhnhDXs43TpcG9z6p3XZFAXSGeSxT8nmWyvPNoan9vw',
-    PRODUCTION: 'rMjwcSJUrS0.cwA.6Gw.vBMKKOrGWxIcb9FpjrdGoRpVbESov5D0V9OUSKuuOk8'
+let SECRET = {
+    STAGING: '', // 'hWhA3IfZkGA.cwA.058.GhnhDXs43TpcG9z6p3XZFAXSGeSxT8nmWyvPNoan9vw',
+    PRODUCTION: '' //'rMjwcSJUrS0.cwA.6Gw.vBMKKOrGWxIcb9FpjrdGoRpVbESov5D0V9OUSKuuOk8'
 };
 
 export const App = (props: AppProps, container: HTMLElement) => {
@@ -15,14 +18,18 @@ export const App = (props: AppProps, container: HTMLElement) => {
     ReactDOM.render(React.createElement(AppContainer, props), container);
 }
 
+function isProduction() {
+    return !(window.location.hostname.indexOf(PRODUCTION_SHORT_URL) === -1);
+}
+
 function uuidv4(): string {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
     });
 }
 
-function getAppProps() : Object {
+function getAppProps(): Object {
     var params = BotChat.queryParams(location.search);
 
     var user = {
@@ -37,7 +44,7 @@ function getAppProps() : Object {
 
     return {
         directLine: {
-            secret: window.location.hostname.indexOf(PRODUCTION_SHORT_URL) === -1 ? SECRET.STAGING : SECRET.PRODUCTION,
+            secret: isProduction() ? SECRET.PRODUCTION : SECRET.STAGING,
             token: params['t'],
             domain: params['domain'],
             webSocket: params['webSocket'] && params['webSocket'] === 'true'
@@ -50,8 +57,59 @@ function getAppProps() : Object {
     }
 }
 
-const AppContainer = (props: AppProps) => {
-    return  <div className="wc-app">
-        <Chat { ...props } {...getAppProps()} />
-    </div>
+export interface ChatState {
+    loggedIn: boolean,
+    loggingIn: boolean,
+    error: boolean
+}
+
+class AppContainer extends React.PureComponent<ChatProps, ChatState> {
+    state: ChatState = {
+        loggedIn: false,
+        error: false,
+        loggingIn: false
+    }
+
+    constructor(p: ChatProps) {
+        super(p);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(username: string, password: string) {
+        this.setState({ loggingIn: true });
+        let url = '';
+        if (isProduction()) {
+            url = '/api/form/secret';
+        } else {
+            url = 'https://gicpublicsitebot-staging.azurewebsites.net/api/form/secret';
+        }
+
+        axios.post(url, {
+            username,
+            password
+        })
+            .then(resp => {
+                if (isProduction()) {
+                    SECRET.PRODUCTION = resp.data;
+                } else {
+                    SECRET.STAGING = resp.data;
+                }
+
+                this.setState({ loggedIn: true, loggingIn: false, error: false });
+            })
+            .catch(error => {
+                this.setState({ loggedIn: false, loggingIn: false, error: true });
+            });
+    }
+
+    render() {
+        if (!this.state.loggedIn) {
+            return <Login loggingIn={this.state.loggingIn} onSubmit={this.handleSubmit} error={this.state.error} />;
+        }
+
+        return <div className="wc-app">
+            <Chat { ...this.props } {...getAppProps() } />
+        </div>
+    }
 }
---
+++ src/Login.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { FormEventHandler } from 'react-dom/node_modules/@types/react';
+
+export interface LoginProps {
+  onSubmit: Function,
+  loggingIn: boolean,
+  error: boolean
+}
+
+export interface LoginState {
+  username: string,
+  password: string
+}
+
+class Login extends React.PureComponent<LoginProps, LoginState> {
+  private onSubmit: Function;
+
+  state: LoginState = {
+    username: '',
+    password: ''
+  }
+
+  constructor(p: LoginProps) {
+    super(p);
+
+    this.hanldeSubmit = this.hanldeSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const name = e.target.name;
+    const nextState = {
+      [name]: e.target.value
+    };
+
+    this.setState(nextState);
+  }
+
+  hanldeSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    this.props.onSubmit(this.state.username, this.state.password);
+  }
+
+  render() {
+    return <main className="wc-login-page">
+      <form onSubmit={this.hanldeSubmit}>
+        <img src="./avatar.png" className="wc-login-avatar" />
+        <h1>Login</h1>
+        <p className="form-group">
+          <label htmlFor="username">Username</label>
+          <input disabled={this.props.loggingIn} required id="username" type="text" value={this.state.username} name="username" placeholder="Enter username..." onChange={this.handleChange} />
+        </p>
+        <p className="form-group">
+          <label htmlFor="password">Password</label>
+          <input disabled={this.props.loggingIn} required id="password" type="password" value={this.state.password} name="password" placeholder="Enter your password..." onChange={this.handleChange} />
+          {this.props.error && <span className="error-message">Password is invalid</span>}
+        </p>
+        <button disabled={this.props.loggingIn || !(this.state.username && this.state.password)}>
+          {this.props.loggingIn ? 'Logging In...' : 'Login'}
+        </button>
+      </form>
+    </main>
+  }
+}
+
+export default Login;
