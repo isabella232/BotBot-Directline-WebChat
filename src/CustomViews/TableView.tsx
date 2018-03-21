452b8cdf5abefbb56c82449dc3fc4d1ade89b21e
luungoc2005 <luungoc2005@gmail.com>
2018-03-21 17:29:01 +0700

    just remove layout:fixed instead

--- src/CustomViews/TableView.tsx
+++ src/CustomViews/TableView.tsx
@@ -22,18 +22,6 @@ export class TableView extends React.Component < TableProps, {} > {
         : null).filter((item : string) => item) // remove null items
       : []
     const hightlightIdx = data.findIndex((item : string) => item === '_highlight')
-    // Get an array of max lengths of strings
-    const maxLengths: Array<number> = data
-      // .slice(1)
-      .reduce((accumulator: Array<number>, currentValue: Array<string>) => 
-        accumulator.map((length: number, rowIdx: number) => 
-          currentValue[rowIdx].length > length ? currentValue[rowIdx].length : length),
-        Array.apply(null, Array(data[0].length)).map(Number.prototype.valueOf, 0))
-    let columnWidths: Array<number> = maxLengths
-      .map((length: number) => Math.log(Math.min(length, 200))) // Max is log(200) = ~5x
-      // .map((length: number) => length > 25 ? 2 : 1) // Hard threshold to increase width by 2 if length exceeds 25
-    const totalWidth: number = columnWidths.reduce((a: number, b: number) => a + b, 0)
-    columnWidths = columnWidths.map(width => Math.round(width * 100 / totalWidth))
 
     return (
       <div className="table-container">
@@ -43,7 +31,7 @@ export class TableView extends React.Component < TableProps, {} > {
               {data.length > 0 && data[0]
               .filter((hcell : string, idx : number) => !optionRows.includes(idx))
               .map((hcell : string, idx : number) => 
-                <th style={{width: `${columnWidths[idx]}%`}} key={idx}>{<FormattedText text={hcell} />}</th>)}
+                <th key={idx}>{<FormattedText text={hcell} />}</th>)}
             </tr>
           </thead>
           <tbody>
