332d9f29c1a68ba4830d087f30798ba95801f3ca
Thang Kieu <kqthang1505@gmail.com>
2018-04-02 14:53:54 +0700

    update parent origin

--- src/Constants.tsx
+++ src/Constants.tsx
@@ -1,4 +1,4 @@
-export const PARENT_ORIGIN = 'http://127.0.0.1:8000';
+export const PARENT_ORIGIN = 'http://localhost:4200';
 export const PRODUCTION_SHORT_URL = 'daimlerbot.azurewebsites.net';
 export const SECRET = {
     STAGING: 'oTdzMuiS_ko.cwA.pmo.RBhSwn9ye1Mwv5FgOdp2RZtWH0u_PSM60yH-znsUc5w',
