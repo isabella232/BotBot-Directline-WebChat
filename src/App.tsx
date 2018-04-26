cd49e079866cb9a39a6c55b70900f86742eaa7c2
Thang Kieu <kqthang1505@gmail.com>
2018-04-26 10:50:01 +0700

    Add config to Chat Widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,43 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Chat, ChatProps } from './Chat';
+import { Chat, ChatProps, IConfig } from './Chat';
 import * as konsole from './Konsole';
 
 import { PRODUCTION_SHORT_URL, SECRET, PARENT_ORIGIN } from './Constants';
 export type AppProps = ChatProps;
 
-
 export const App = (props: AppProps, container: HTMLElement) => {
-    konsole.log("BotChat.App props", props);
+    konsole.log('BotChat.App props', props);
     ReactDOM.render(React.createElement(AppContainer, props), container);
-}
+};
 
 function uuidv4(): string {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        var r = (Math.random() * 16) | 0,
+            v = c == 'x' ? r : (r & 0x3) | 0x8;
         return v.toString(16);
     });
 }
 
-function getAppProps() : Object {
+function getAppProps(): Object {
     var params = BotChat.queryParams(location.search);
 
     var user = {
         id: params['userid'] || uuidv4(),
-        name: params["username"] || 'user'
+        name: params['username'] || 'user'
     };
 
     var bot = {
         id: params['botid'] || 'botid',
-        name: params["botname"] || 'botname'
+        name: params['botname'] || 'botname'
     };
 
     return {
         directLine: {
-            secret: window.location.hostname.indexOf(PRODUCTION_SHORT_URL) === -1 ? SECRET.STAGING : SECRET.PRODUCTION,
+            secret:
+                window.location.hostname.indexOf(PRODUCTION_SHORT_URL) === -1
+                    ? SECRET.STAGING
+                    : SECRET.PRODUCTION,
             token: params['t'],
             domain: params['domain'],
             webSocket: params['webSocket'] && params['webSocket'] === 'true'
@@ -44,11 +47,33 @@ function getAppProps() : Object {
         locale: params['locale'],
         resize: 'window'
         // sendTyping: true,    // defaults to false. set to true to send 'typing' activities to bot (and other users) when user is typing
-    }
+    };
 }
 
+const compileStyle = (config: IConfig) => {
+    let style;
+    if (config.fontUrl) {
+        style = config.style;
+        const styleTag = document.createElement('style');
+        styleTag.innerHTML = `
+        @import url(${config.fontUrl});
+        .wc-message { border-color: ${config.color} }
+        .wc-console #wc-send-icon { fill: ${config.color} }
+        `;
+        document.head.appendChild(styleTag);
+    }
+};
+
 const AppContainer = (props: AppProps) => {
-    return  <div className="wc-app">
-        <Chat { ...props } {...getAppProps()} />
-    </div>
-}
+    let style;
+    if (props.config && props.config.fontUrl) {
+        style = props.config.style;
+        compileStyle(props.config);
+    }
+
+    return (
+        <div className="wc-app" style={style}>
+            <Chat {...props} {...getAppProps()} />
+        </div>
+    );
+};
--- src/Chat.tsx
+++ src/Chat.tsx
@@ -5,7 +5,14 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
-import { Activity, IBotConnection, User, DirectLine, DirectLineOptions, CardActionTypes } from 'botframework-directlinejs';
+import {
+    Activity,
+    IBotConnection,
+    User,
+    DirectLine,
+    DirectLineOptions,
+    CardActionTypes
+} from 'botframework-directlinejs';
 import { createStore, ChatActions, sendMessage } from './Store';
 import { Provider } from 'react-redux';
 import { SpeechOptions } from './SpeechOptions';
@@ -19,36 +26,44 @@ import { PARENT_ORIGIN } from './Constants';
 import { initSignalR } from './CustomSocket';
 
 export interface IDaimlerData {
-    okURL: string,
-    urlFlag: string,
-    language: string,
-    configurationDate: string,
-    orderingMarket: string,
-    pricingDate: string,
-    salesLevel: string,
-    taxDate: string,
-    materialType: string,
-    plant: string,
-    baumuster: string,
-    internalVehicleId: string,
-    savedConfig: string,
-    productionDate: string,
-    vehicleDescription: string,
-    vehicleClass: string,
-    preSeriesModeFlag: string
+    okURL: string;
+    urlFlag: string;
+    language: string;
+    configurationDate: string;
+    orderingMarket: string;
+    pricingDate: string;
+    salesLevel: string;
+    taxDate: string;
+    materialType: string;
+    plant: string;
+    baumuster: string;
+    internalVehicleId: string;
+    savedConfig: string;
+    productionDate: string;
+    vehicleDescription: string;
+    vehicleClass: string;
+    preSeriesModeFlag: string;
+}
+
+export interface IConfig {
+    fontUrl?: string;
+    style?: object;
+    logo?: string;
+    color?: string
 }
 
 export interface ChatProps {
-    user: User,
-    bot: User,
-    botConnection?: IBotConnection,
-    directLine?: DirectLineOptions,
-    speechOptions?: SpeechOptions,
-    locale?: string,
-    selectedActivity?: BehaviorSubject<ActivityOrID>,
-    sendTyping?: boolean,
-    formatOptions?: FormatOptions,
-    resize?: 'none' | 'window' | 'detect'
+    user: User;
+    bot: User;
+    botConnection?: IBotConnection;
+    directLine?: DirectLineOptions;
+    speechOptions?: SpeechOptions;
+    locale?: string;
+    selectedActivity?: BehaviorSubject<ActivityOrID>;
+    sendTyping?: boolean;
+    formatOptions?: FormatOptions;
+    resize?: 'none' | 'window' | 'detect';
+    config?: IConfig;
 }
 
 import { History } from './History';
@@ -75,25 +90,35 @@ export class Chat extends React.Component<ChatProps, {}> {
     constructor(props: ChatProps) {
         super(props);
 
-        konsole.log("BotChat.Chat props", props);
+        konsole.log('BotChat.Chat props', props);
 
         this.store.dispatch<ChatActions>({
             type: 'Set_Locale',
-            locale: props.locale || (window.navigator as any)["userLanguage"] || window.navigator.language || 'en'
+            locale:
+                props.locale ||
+                (window.navigator as any)['userLanguage'] ||
+                window.navigator.language ||
+                'en'
         });
 
         if (props.formatOptions)
-            this.store.dispatch<ChatActions>({ type: 'Set_Format_Options', options: props.formatOptions });
+            this.store.dispatch<ChatActions>({
+                type: 'Set_Format_Options',
+                options: props.formatOptions
+            });
 
         if (props.sendTyping)
-            this.store.dispatch<ChatActions>({ type: 'Set_Send_Typing', sendTyping: props.sendTyping });
+            this.store.dispatch<ChatActions>({
+                type: 'Set_Send_Typing',
+                sendTyping: props.sendTyping
+            });
 
         if (props.speechOptions) {
             Speech.SpeechRecognizer.setSpeechRecognizer(props.speechOptions.speechRecognizer);
             Speech.SpeechSynthesizer.setSpeechSynthesizer(props.speechOptions.speechSynthesizer);
         }
 
-        this.receiveMessage = this.receiveMessage.bind(this)
+        this.receiveMessage = this.receiveMessage.bind(this);
         window.addEventListener('message', this.receiveMessage, false);
     }
 
@@ -111,17 +136,23 @@ export class Chat extends React.Component<ChatProps, {}> {
     private handleIncomingActivity(activity: Activity) {
         let state = this.store.getState();
         switch (activity.type) {
-            case "message":
+            case 'message':
                 const isUserMessage = activity.from.id === state.connection.user.id;
                 if (!isUserMessage && this.typingActivity) {
                     // Clear typing whenever receive any bot response
-                    this.store.dispatch<ChatActions>({ type: 'Clear_Typing', id: this.typingActivity.id });
+                    this.store.dispatch<ChatActions>({
+                        type: 'Clear_Typing',
+                        id: this.typingActivity.id
+                    });
                 }
 
-                this.store.dispatch<ChatActions>({ type: isUserMessage ? 'Receive_Sent_Message' : 'Receive_Message', activity });
+                this.store.dispatch<ChatActions>({
+                    type: isUserMessage ? 'Receive_Sent_Message' : 'Receive_Message',
+                    activity
+                });
                 break;
 
-            case "typing":
+            case 'typing':
                 if (activity.from.id !== state.connection.user.id) {
                     this.typingActivity = activity;
                     this.store.dispatch<ChatActions>({ type: 'Show_Typing', activity });
@@ -143,9 +174,9 @@ export class Chat extends React.Component<ChatProps, {}> {
         const tabIndex = getTabIndex(target);
 
         if (
-            target === findDOMNode(this.chatviewPanel)
-            || typeof tabIndex !== 'number'
-            || tabIndex < 0
+            target === findDOMNode(this.chatviewPanel) ||
+            typeof tabIndex !== 'number' ||
+            tabIndex < 0
         ) {
             evt.stopPropagation();
 
@@ -172,36 +203,49 @@ export class Chat extends React.Component<ChatProps, {}> {
 
         const botConnection = this.props.directLine
             ? (this.botConnection = new DirectLine(this.props.directLine))
-            : this.props.botConnection
-            ;
+            : this.props.botConnection;
 
-        if (this.props.resize === 'window')
-            window.addEventListener('resize', this.resizeListener);
+        if (this.props.resize === 'window') window.addEventListener('resize', this.resizeListener);
 
-        this.store.dispatch<ChatActions>({ type: 'Start_Connection', user: this.props.user, bot: this.props.bot, botConnection, selectedActivity: this.props.selectedActivity });
+        this.store.dispatch<ChatActions>({
+            type: 'Start_Connection',
+            user: this.props.user,
+            bot: this.props.bot,
+            botConnection,
+            selectedActivity: this.props.selectedActivity
+        });
 
-        this.connectionStatusSubscription = botConnection.connectionStatus$.subscribe(connectionStatus =>{
-                if(this.props.speechOptions && this.props.speechOptions.speechRecognizer){
+        this.connectionStatusSubscription = botConnection.connectionStatus$.subscribe(
+            connectionStatus => {
+                if (this.props.speechOptions && this.props.speechOptions.speechRecognizer) {
                     let refGrammarId = botConnection.referenceGrammarId;
-                    if(refGrammarId)
+                    if (refGrammarId)
                         this.props.speechOptions.speechRecognizer.referenceGrammarId = refGrammarId;
                 }
-                this.store.dispatch<ChatActions>({ type: 'Connection_Change', connectionStatus })
+                this.store.dispatch<ChatActions>({ type: 'Connection_Change', connectionStatus });
             }
         );
 
         this.activitySubscription = botConnection.activity$.subscribe(
             activity => this.handleIncomingActivity(activity),
-            error => konsole.log("activity$ error", error)
+            error => konsole.log('activity$ error', error)
         );
 
         if (this.props.selectedActivity) {
-            this.selectedActivitySubscription = this.props.selectedActivity.subscribe(activityOrID => {
-                this.store.dispatch<ChatActions>({
-                    type: 'Select_Activity',
-                    selectedActivity: activityOrID.activity || this.store.getState().history.activities.find(activity => activity.id === activityOrID.id)
-                });
-            });
+            this.selectedActivitySubscription = this.props.selectedActivity.subscribe(
+                activityOrID => {
+                    this.store.dispatch<ChatActions>({
+                        type: 'Select_Activity',
+                        selectedActivity:
+                            activityOrID.activity ||
+                            this.store
+                                .getState()
+                                .history.activities.find(
+                                    activity => activity.id === activityOrID.id
+                                )
+                    });
+                }
+            );
         }
 
         initSignalR(this.props.user.id);
@@ -210,10 +254,8 @@ export class Chat extends React.Component<ChatProps, {}> {
     componentWillUnmount() {
         this.connectionStatusSubscription.unsubscribe();
         this.activitySubscription.unsubscribe();
-        if (this.selectedActivitySubscription)
-            this.selectedActivitySubscription.unsubscribe();
-        if (this.botConnection)
-            this.botConnection.end();
+        if (this.selectedActivitySubscription) this.selectedActivitySubscription.unsubscribe();
+        if (this.botConnection) this.botConnection.end();
         window.removeEventListener('resize', this.resizeListener);
     }
 
@@ -227,30 +269,32 @@ export class Chat extends React.Component<ChatProps, {}> {
 
         // only render real stuff after we know our dimensions
         let header: JSX.Element;
-        if (state.format.options.showHeader) header =
-            <div className="wc-header">
-                <img src="./avatar.png" />
-                <h1>{ state.format.strings.title }</h1>
-            </div>;
+        if (state.format.options.showHeader)
+            header = (
+                <div className="wc-header">
+                    <img src={this.props.logo || './avatar.png'} />
+                    <h1>{state.format.strings.title}</h1>
+                </div>
+            );
 
         let resize: JSX.Element;
-        if (this.props.resize === 'detect') resize =
-            <ResizeDetector onresize={ this.resizeListener } />;
+        if (this.props.resize === 'detect')
+            resize = <ResizeDetector onresize={this.resizeListener} />;
 
         return (
-            <Provider store={ this.store }>
+            <Provider store={this.store}>
                 <div
                     className="wc-chatview-panel"
-                    onKeyDownCapture={ this._handleKeyDownCapture }
-                    ref={ div => this.chatviewPanel = div }
-                    tabIndex={ 0 }
+                    onKeyDownCapture={this._handleKeyDownCapture}
+                    ref={div => (this.chatviewPanel = div)}
+                    tabIndex={0}
                 >
-                    { header }
+                    {header}
                     <MessagePane>
                         <History />
                     </MessagePane>
-                    <Shell ref={ this._saveShellRef } />
-                    { resize }
+                    <Shell ref={this._saveShellRef} />
+                    {resize}
                 </div>
             </Provider>
         );
@@ -265,87 +309,101 @@ export const doCardAction = (
     botConnection: IBotConnection,
     from: User,
     locale: string,
-    sendMessage: (value: string, user: User, locale: string) => void,
-): IDoCardAction => (
-    type,
-    actionValue
-) => {
-
-    const text = (typeof actionValue === 'string') ? actionValue as string : undefined;
-    const value = (typeof actionValue === 'object')? actionValue as object : undefined;
+    sendMessage: (value: string, user: User, locale: string) => void
+): IDoCardAction => (type, actionValue) => {
+    const text = typeof actionValue === 'string' ? (actionValue as string) : undefined;
+    const value = typeof actionValue === 'object' ? (actionValue as object) : undefined;
 
     switch (type) {
-        case "imBack":
-            if (typeof text === 'string')
-                sendMessage(text, from, locale);
+        case 'imBack':
+            if (typeof text === 'string') sendMessage(text, from, locale);
             break;
 
-        case "postBack":
+        case 'postBack':
             sendPostBack(botConnection, text, value, from, locale);
             break;
 
-        case "call":
-        case "openUrl":
-        case "playAudio":
-        case "playVideo":
-        case "showImage":
-        case "downloadFile":
-        case "signin":
+        case 'call':
+        case 'openUrl':
+        case 'playAudio':
+        case 'playVideo':
+        case 'showImage':
+        case 'downloadFile':
+        case 'signin':
             window.open(text);
             break;
 
         default:
-            konsole.log("unknown button type", type);
-        }
-}
+            konsole.log('unknown button type', type);
+    }
+};
 
-export const sendPostBack = (botConnection: IBotConnection, text: string, value: object, from: User, locale: string) => {
-    botConnection.postActivity({
-        type: "message",
-        text,
-        value,
-        from,
-        locale
-    })
-    .subscribe(id => {
-        konsole.log("success sending postBack", id)
-    }, error => {
-        konsole.log("failed to send postBack", error);
-    });
-}
+export const sendPostBack = (
+    botConnection: IBotConnection,
+    text: string,
+    value: object,
+    from: User,
+    locale: string
+) => {
+    botConnection
+        .postActivity({
+            type: 'message',
+            text,
+            value,
+            from,
+            locale
+        })
+        .subscribe(
+            id => {
+                konsole.log('success sending postBack', id);
+            },
+            error => {
+                konsole.log('failed to send postBack', error);
+            }
+        );
+};
 
-export const renderIfNonempty = (value: any, renderer: (value: any) => JSX.Element ) => {
+export const renderIfNonempty = (value: any, renderer: (value: any) => JSX.Element) => {
     if (value !== undefined && value !== null && (typeof value !== 'string' || value.length > 0))
         return renderer(value);
-}
+};
 
-export const classList = (...args:(string | boolean)[]) => {
+export const classList = (...args: (string | boolean)[]) => {
     return args.filter(Boolean).join(' ');
-}
+};
 
 // note: container of this element must have CSS position of either absolute or relative
-const ResizeDetector = (props: {
-    onresize: () => void
-}) =>
+const ResizeDetector = (props: { onresize: () => void }) => (
     // adapted to React from https://github.com/developit/simple-element-resize-detector
     <iframe
-        style={ { position: 'absolute', left: '0', top: '-100%', width: '100%', height: '100%', margin: '1px 0 0', border: 'none', opacity: 0, visibility: 'hidden', pointerEvents: 'none' } }
-        ref={ frame => {
-            if (frame)
-                frame.contentWindow.onresize = props.onresize;
-        } }
-    />;
+        style={{
+            position: 'absolute',
+            left: '0',
+            top: '-100%',
+            width: '100%',
+            height: '100%',
+            margin: '1px 0 0',
+            border: 'none',
+            opacity: 0,
+            visibility: 'hidden',
+            pointerEvents: 'none'
+        }}
+        ref={frame => {
+            if (frame) frame.contentWindow.onresize = props.onresize;
+        }}
+    />
+);
 
 // For auto-focus in some browsers, we synthetically insert keys into the chatbox.
 // By default, we insert keys when:
 // 1. evt.key.length === 1 (e.g. "1", "A", "=" keys), or
 // 2. evt.key is one of the map keys below (e.g. "Add" will insert "+", "Decimal" will insert ".")
 const INPUTTABLE_KEY: { [key: string]: string } = {
-    Add: '+',      // Numpad add key
-    Decimal: '.',  // Numpad decimal key
-    Divide: '/',   // Numpad divide key
+    Add: '+', // Numpad add key
+    Decimal: '.', // Numpad decimal key
+    Divide: '/', // Numpad divide key
     Multiply: '*', // Numpad multiply key
-    Subtract: '-'  // Numpad subtract key
+    Subtract: '-' // Numpad subtract key
 };
 
 function inputtableKey(key: string) {
