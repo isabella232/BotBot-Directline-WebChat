58ab4421dae58dc52632cb42503c83f23a30b165
Thang Kieu <kqthang1505@gmail.com>
2018-05-17 16:00:37 +0700

    Implement UI Configuration from dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Chat, ChatProps } from './Chat';
 import * as konsole from './Konsole';
+import axios from 'axios';
 
 export type AppProps = ChatProps;
 const PRODUCTION_SHORT_URL = 'canonbot.azurewebsites.net';
@@ -9,6 +10,8 @@ const SECRET = {
     STAGING: 'vHJJa7VhBGQ.cwA.MPM.eWdF_cJu2ZPoE-weTDWzWvbmJOoHGz4OPDai9nlkGwA',
     PRODUCTION: 'Gq5EM3rwkfU.cwA.Fp8.GBNJR72aRCX01jLRgdhcc4_hNUaMSWmry3PiJS__K2E'
 };
+const DASHBOARD_API_URL = ' https://botbotapi-staging.azurewebsites.net/api';
+const BOT_API_URL = 'http://daimlerbot-staging.azurewebsites.net/api'
 
 export const App = (props: AppProps, container: HTMLElement) => {
     konsole.log("BotChat.App props", props);
@@ -50,7 +53,64 @@ function getAppProps() : Object {
     }
 }
 
+export interface IConfig {
+  fontUrl: string,
+  fontFamily: string,
+  textColor: string,
+  brandColor: string,
+  headerBg: string,
+  textProfileColor: string,
+  logo: string
+}
+
+const compileStyle = (config: IConfig) => {
+  let style;
+
+  const styleTag = document.createElement('style');
+  styleTag.innerHTML = `
+    @import url(${config.fontUrl});
+    body .wc-app {
+      font-family: "${config.fontFamily}", sans-serif;
+      color: ${config.textColor};
+    }
+    .wc-message { border-color: ${config.brandColor} }
+    .wc-console #wc-send-icon { fill: ${config.brandColor} }
+    .wc-adaptive-card * {
+      font-family: inherit !important;
+      color: inherit !important;
+    }
+    .wc-card button {
+      color: ${config.brandColor} !important;
+    }
+    .wc-header {
+      background-color: ${config.headerBg};
+      color: ${config.brandColor}
+    }
+    .wc-message-meta { color: ${config.textProfileColor} }
+    .wc-suggested-actions .wc-hscroll > ul > li button { color: ${config.brandColor} }
+    `;
+
+  document.head.appendChild(styleTag);
+
+  // change logo
+  const logoEl = document.querySelector('#BotChatWindow .wc-chatview-panel .wc-header img');
+
+  if (config.logo) {
+    logoEl.setAttribute('src', config.logo);
+  }
+};
+
+function requestCustomiseUI() {
+  axios.get(`${BOT_API_URL}/dashboard/botid`)
+    .then(resp => axios.get(`${DASHBOARD_API_URL}/feconfig/get?botid=${resp.data.id}`))
+    .then(resp => {
+      compileStyle(resp.data);
+    });
+}
+
 const AppContainer = (props: AppProps) => {
+  requestCustomiseUI();
+
     return  <div className="wc-app">
         <Chat { ...props } {...getAppProps()} />
     </div>
